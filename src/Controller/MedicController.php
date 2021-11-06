2691285d4f81716d4a33c0979e602c5f219c11cf
Enrico Garaiman <enrico7899@yahoo.com>
2021-11-06 17:21:47 +0200

    Feature: Medic profile update

--- src/Controller/MedicController.php
+++ src/Controller/MedicController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Medic;
+use App\Form\MedicProfileFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,22 +12,28 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MedicController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/medic/{id}/profil", name="medic_profile")
      */
     public function pacientProfile(Medic $medic, Request $request): Response
     {
-//        $form = $this->createForm(PacientProfileFormType::class, $pacient);
-//
-//        $form->handleRequest($request);
-//        if ($form->isSubmitted() && $form->isValid()) {
-//            $pacientUpdate = $form->getData();
-//            $this->entityManager->persist($pacientUpdate);
-//            $this->entityManager->flush();
-//        }
+        $form = $this->createForm(MedicProfileFormType::class, $medic);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $medicUpdate = $form->getData();
+            $this->entityManager->persist($medicUpdate);
+            $this->entityManager->flush();
+        }
 
         return $this->render('medic/profile.html.twig', [
-//            'form'=>$form->createView()
+            'form'=>$form->createView()
         ]);
     }
 }
--- src/Form/MedicProfileFormType.php
+++ src/Form/MedicProfileFormType.php
@@ -2,50 +2,34 @@
 
 namespace App\Form;
 
-use App\Entity\Pacient;
+use App\Entity\Medic;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-
-class PacientProfileFormType extends AbstractType
+class MedicProfileFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('prenumePacient', TextType::class, [
+            ->add('prenumeMedic', TextType::class, [
                 'row_attr' => ['class' => 'input'],
                 'label' => 'Prenume'
             ])
-            ->add('numePacient', TextType::class, [
+            ->add('numeMedic', TextType::class, [
                 'row_attr' => ['class' => 'input'],
                 'label' => 'Nume'
-            ])
-            ->add('cnp', TextType::class, [
-                'row_attr' => ['class' => 'input'],
-                'attr' => ['readonly'=>true],
-                'label' => 'CNP'
             ])
             ->add('email', EmailType::class, [
                 'row_attr' => ['class' => 'input'],
                 'attr' => ['readonly'=>true],
                 'label' => 'Email'
             ])
-            ->add('asigurare', ChoiceType::class, [
-                'choices' => [
-                    'Nu' => 0,
-                    'Da' => 1,
-                ],
+            ->add('specializare', TextType::class, [
                 'row_attr' => ['class' => 'input'],
-                'label' => 'Asigurare',
-                'label_attr' => ['class' => 'select2-label']
-            ])
-            ->add('adresa', TextType::class, [
-                'row_attr' => ['class' => 'input'],
-                'label' => 'Adresă'
+                'label' => 'Specializare'
             ])
         ;
     }
@@ -53,7 +37,7 @@ class PacientProfileFormType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Pacient::class,
+            'data_class' => Medic::class,
         ]);
     }
 }
