c379618d857b6548221e1051c6845db67d162d4c
Enrico Garaiman <enrico7899@yahoo.com>
2021-11-06 19:08:05 +0200

    Feature: add confirmation sweet alert in base

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -13,4 +13,23 @@ $(document).ready(function () {
             timer: 2000
         })
     }
+
+    // Request confirmation when submit form
+    $('.request-confirmation').on('click', function (e) {
+        e.preventDefault()
+        Swal.fire({
+            title: $(this).attr('message'),
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#0c4079',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Confirmă',
+            cancelButtonText: 'Anulează',
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $(this).parents('form:first').submit();
+            }
+        })
+    })
 });
